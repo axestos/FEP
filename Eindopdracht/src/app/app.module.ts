e3c59b13bf7be5ec488b1d13b691a6f8f989cc4a
akatar1 <kevin.w@live.nl>
2017-11-04 15:27:34 +0100

    Firebase zit nu in de app

--- Eindopdracht/src/app/app.module.ts
+++ Eindopdracht/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAfWuTAVavRZTEnYczfxiIzLRVVAG40HD0",
+  authDomain: "fep-project-31319.firebaseapp.com",
+  databaseURL: "https://fep-project-31319.firebaseio.com",
+  projectId: "fep-project-31319",
+  storageBucket: "fep-project-31319.appspot.com",
+  messagingSenderId: "91699660492"
+ };
+
 
 @NgModule({
   declarations: [
@@ -14,7 +24,8 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
