2347efd91998743017c5f08996e5e677048cd686
evantjuh <hardpop_radio@hotmail.com>
2017-11-04 17:21:05 +0100

    header

--- Eindopdracht/src/app/header/header.component.ts
+++ Eindopdracht/src/app/header/header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  docent = false;
+  username = 'J.Jansen';
+
   constructor() { }
 
   ngOnInit() {
