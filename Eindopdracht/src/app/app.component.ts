35d9b9a082f92242ff010d59012b70c8b340c4e8
Deva Vrijma <dacvrijma@gmail.com>
2017-11-04 14:30:30 +0100

    We krijgen wit scherm

--- Eindopdracht/src/app/app.component.ts
+++ Eindopdracht/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-login',
+  templateUrl: './login/login.component.html',
+  styleUrls: ['./login/login.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Login';
 }
--- Eindopdracht/src/app/app.module.ts
+++ Eindopdracht/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
