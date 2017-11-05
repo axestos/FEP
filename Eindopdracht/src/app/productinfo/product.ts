5398fcc1c0a078c10c7ba261b62ac78151d7ae87
Deva Vrijma <dacvrijma@gmail.com>
2017-11-06 00:35:41 +0100

    Productinfo werkt voor een groot deel!

---
+++ Eindopdracht/src/app/productinfo/product.ts
@@ -0,0 +1,7 @@
+export class product {
+    naam: string;
+    omschrijving:string;
+    imgLocation:string;
+    voorraad:number;
+    maxLeentijdDagen:number;
+}
--- Eindopdracht/src/app/productinfo/productinfo.service.ts
+++ Eindopdracht/src/app/productinfo/productinfo.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../auth/auth.service';
+import { product } from './product';
 
 @Injectable()
 export class ProductinfoService{
@@ -13,12 +14,15 @@ export class ProductinfoService{
     }
     
 
-    loadData(id: string, item: {productNaam: string}) {
+    loadData(id: string, product: product) {
       // var that = this;
-        var product = firebase.database().ref("/producten/"+id+"/").once('value').then(function(snapshot) {
-          console.log(snapshot.child("productNaam").val())
-          item = {productNaam:snapshot.child("productNaam").val()};
-          console.log(item);
+        var data = firebase.database().ref("/producten/"+id+"/").once('value').then(function(snapshot) {
+          product.imgLocation = snapshot.child("imgLocation").val();
+          product.maxLeentijdDagen = snapshot.child("maxLeentijd").val();
+          product.naam = snapshot.child("productNaam").val();
+          product.omschrijving = snapshot.child("productOmschrijving").val();
+          product.voorraad = snapshot.child("productVoorraad").val();
+          console.log(product);
       });
     }
 
