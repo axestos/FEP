6244e48fe3623adce606c4e6499cd964b1901477
riskaddict <stevensnippe@live.nl>
2017-11-06 16:42:34 +0100

    hardware OO

--- Eindopdracht/src/app/beschikbarehardware/beschikbarehardware.component.ts
+++ Eindopdracht/src/app/beschikbarehardware/beschikbarehardware.component.ts
@@ -11,25 +11,9 @@ import { HardwareService} from '../beschikbarehardware/hardware.service';
 export class BeschikbarehardwareComponent implements OnInit {
 
   public hardwareList:hardware[]=[];
-  public producten : {imgLocation : string, maxLeenTijd : number; productNaam : string; productOmschrijving : string; productVoorraad : number;}[] = [];
-
 
   constructor(public hardwareService: HardwareService) {
-    this.hardwareList[0] = new hardware();
-    this.hardwareList[0].naam = "Arduino";
-    this.hardwareList[0].omschrijving = "ArduinoDesc";
-    this.hardwareList[0].imgLocation = "https://upload.wikimedia.org/wikipedia/commons/3/38/Arduino_Uno_-_R3.jpg";
-    this.hardwareList[0].voorraad = 100;
-    this.hardwareList[0].maxLeentijdDagen = 10;
-
-    this.hardwareList[1] = new hardware();
-    this.hardwareList[1].naam = "Arduino mini";
-    this.hardwareList[1].omschrijving = "ESPDesc";
-    this.hardwareList[1].imgLocation = "http://media.rs-online.com/t_large/F7617327-01.jpg";
-    this.hardwareList[1].voorraad = 66;
-    this.hardwareList[1].maxLeentijdDagen = 6;
-
-    this.producten = hardwareService.producten;
+    this.hardwareList = hardwareService.hardwareList;
 
   }
 
--- Eindopdracht/src/app/beschikbarehardware/hardware.ts
+++ Eindopdracht/src/app/beschikbarehardware/hardware.ts
@@ -4,4 +4,16 @@ export class hardware {
     imgLocation:string;
     voorraad:number;
     maxLeentijdDagen:number;
+
+    /*constructor(naam: string, omschrijving:string, imgLocation: string, voorraad:number, maxLeentijdDagen:number) {
+      this.naam = naam;
+      this.omschrijving = omschrijving;
+      this.imgLocation = imgLocation;
+      this.voorraad = voorraad;
+      this.maxLeentijdDagen = maxLeentijdDagen;
+    }*/
+
+    constructor(){
+
+    }
 }
